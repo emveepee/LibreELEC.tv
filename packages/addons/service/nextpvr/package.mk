003f429982bcd81c5ab612555fceb28106d1a1ee
Martin Vallevand <mvallevand@gmail.com>
2025-10-30 13:39:32 -0400

    [le12] nextpvr: Update to 7.0.3
    LE 12 version of PR 10625

--- packages/addons/service/nextpvr/package.mk
+++ packages/addons/service/nextpvr/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="nextpvr"
-PKG_VERSION="7.0.1~Omega"
-PKG_ADDON_VERSION="7.0.1~2"
+PKG_VERSION="7.0.3~Omega"
+PKG_ADDON_VERSION="7.0.3~3"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="NextPVR"
